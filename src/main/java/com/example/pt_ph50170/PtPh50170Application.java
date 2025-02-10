14b2f69e0530e4a14ff89c4c615b2001dddaca35
toanf2106 <phamtoan17tb@gmail.com>
2025-02-10 13:12:12 +0700

    lần cam kết đầu tiên

---
+++ src/main/java/com/example/pt_ph50170/PtPh50170Application.java
@@ -0,0 +1,13 @@
+package com.example.pt_ph50170;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtPh50170Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PtPh50170Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/pt_ph50170/PtPh50170ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pt_ph50170;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtPh50170ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
